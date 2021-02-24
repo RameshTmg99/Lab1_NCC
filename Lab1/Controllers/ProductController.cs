71bd335a6db70ace070a69a24318a50201f70afd
RameshTmg99 <rameshtamang4441@gmail.com>
2021-02-24 11:12:16 +0545

    Add project files.

---
+++ Lab1/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductController : Controller
+    {
+        DataContext dbcontext = new DataContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var data = dbcontext.products.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ Lab1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string Company { get; set; }
+
+        //Collection navigation property
+        //public ICollection<Employee> employees { get; set; }
+    }
+}
