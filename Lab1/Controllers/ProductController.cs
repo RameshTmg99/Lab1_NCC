bd49e145ecab7d60544236e497f078ddfde0a5c5
RameshTmg99 <rameshtamang4441@gmail.com>
2021-02-25 10:38:53 +0545

    2021/02/25

--- Lab1/Controllers/ProductController.cs
+++ Lab1/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ namespace Lab1.Controllers
             parameter.Add(p.SerialNumber);
             parameter.Add(p.Company);
             object[] parameterArray = parameter.ToArray();
-            int output = dbcontext.Database.ExecuteSqlCommand("INSERT INTO products(name,serailNumber,company)VALUES(@p0,@p1,@p2)", parameterArray);
+            int output = dbcontext.Database.ExecuteSqlCommand("INSERT INTO products(name,serialNumber,company)VALUES(@p0,@p1,@p2)", parameterArray);
             if(output>0)
             {
                 ViewBag.success = "Data added Successfully";
@@ -40,6 +40,46 @@ namespace Lab1.Controllers
                 ViewBag.error = "Data not added !!";
             }
             return View();
+        }
+        public ActionResult details(int id)
+        {
+            var data = dbcontext.products.SqlQuery("SELECT * FROM products where id=@p0", id).SingleOrDefault();
+            return View(data);       
+        }
+
+        public ActionResult delete(int id)
+        {
+            var data = dbcontext.Database.ExecuteSqlCommand("DELETE FROM products where id=@p0", id);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult edit(int id)
+        {
+            var data = dbcontext.products.SqlQuery("SELECT * FROM products where id = @p0", id).SingleOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult edit(Product p)
+        {
+            List<object> parameter = new List<object>();
+            parameter.Add(p.Name);
+            parameter.Add(p.SerialNumber);
+            parameter.Add(p.Company);
+            parameter.Add(p.Id);
+            object[] parameterArray = parameter.ToArray();
+            int output = dbcontext.Database.ExecuteSqlCommand("UPDATE products set name=@p0,serialNumber=@p1,company=@p2 where id=@p3", parameterArray);
+            if (output > 0)
+            {
+                ViewBag.success = "Data Updated Successfully";
+            }
+            else
+            {
+                ViewBag.error = "Data not Updated !!";
+            }
+            return RedirectToAction("index"); ;
+
         }
     }
 }
